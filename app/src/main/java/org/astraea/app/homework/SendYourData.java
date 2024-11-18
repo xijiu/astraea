2d9a0a136f2c5c9c618d9c04657e15d3a70b684d
xijiu <422766572@qq.com>
2024-11-18 10:09:45 +0800

    test

--- app/src/main/java/org/astraea/app/homework/SendYourData.java
+++ app/src/main/java/org/astraea/app/homework/SendYourData.java
@@ -170,7 +170,7 @@ public class SendYourData {
                   ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
                   bootstrapServers,
                   ProducerConfig.COMPRESSION_TYPE_CONFIG,
-                  "gzip"),
+                  "snappy"),
               serializer,
               new ByteArraySerializer());
     }
