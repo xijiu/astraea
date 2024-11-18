c3850781afcc67e5f24dcce1e492ef29e17b3b5c
xijiu <422766572@qq.com>
2024-11-18 11:40:33 +0800

    test

--- app/src/main/java/org/astraea/app/homework/SendYourData.java
+++ app/src/main/java/org/astraea/app/homework/SendYourData.java
@@ -172,7 +172,7 @@ public class SendYourData {
                   ProducerConfig.COMPRESSION_TYPE_CONFIG,
                   "gzip",
                   ProducerConfig.BUFFER_MEMORY_CONFIG,
-                  "8388608",
+                  "16777216",
                   ProducerConfig.BATCH_SIZE_CONFIG,
                   "4194304",
                   ProducerConfig.LINGER_MS_CONFIG,
