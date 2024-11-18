c9c12686fdd7f177d14dc20bf55d33ce012e183d
xijiu <422766572@qq.com>
2024-11-18 11:43:49 +0800

    test

--- app/src/main/java/org/astraea/app/homework/SendYourData.java
+++ app/src/main/java/org/astraea/app/homework/SendYourData.java
@@ -174,7 +174,7 @@ public class SendYourData {
                   ProducerConfig.BUFFER_MEMORY_CONFIG,
                   "16777216",
                   ProducerConfig.BATCH_SIZE_CONFIG,
-                  "4194304",
+                  "1048576",
                   ProducerConfig.LINGER_MS_CONFIG,
                   "2000"),
               serializer,
